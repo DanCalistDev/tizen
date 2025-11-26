278168dfe19d8d1cd485e335f5fe9099e534ff5e
= <=>
2025-11-26 13:44:24 -0300

    chore: update webpack to output to root

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 module.exports = {
   entry: './src/index.tsx',
   output: {
-    path: path.resolve(__dirname, 'docs'),
+    path: path.resolve(__dirname),
     filename: 'bundle.js'
   },
   module: {
